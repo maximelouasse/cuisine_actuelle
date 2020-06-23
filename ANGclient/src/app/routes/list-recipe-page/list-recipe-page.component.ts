41120eedc091d2b64087414cb10fe2d3a93e43ba
Maxime Louasse <m.louasse@neofly.com>
2020-06-23 09:36:43 +0200

    Start page List Recipe

--- ANGclient/src/app/routes/list-recipe-page/list-recipe-page.component.ts
+++ ANGclient/src/app/routes/list-recipe-page/list-recipe-page.component.ts
@@ -1,29 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RecipeService } from "../../services/recipes/recipe.service";
+import { ObservablesService } from "../../services/observable/observable.service";
 
 @Component({
   selector: 'app-list-recipe-page',
   templateUrl: './list-recipe-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./list-recipe-page.component.css']
 })
 export class ListRecipePageComponent implements OnInit {
 
   constructor(
-    private RecipeService: RecipeService
+    private RecipeService: RecipeService,
+    private ObservablesService: ObservablesService
   ) { }
 
   public allRecipe: any;
+  public listFilterEvent: any;
+  public listFilterType: any;
+
+  /**
+   * Methods
+   */
+    public convertSecondToMinute = (second: number) => {
+      return Math.floor(second / 60);
+    }
+  //
 
   ngOnInit(): void {
+    this.RecipeService.getListFilter("event")
+      .then(apiResponse => {
+        this.listFilterEvent = apiResponse.data;
+      })
+      .catch(error => {
+        console.log('ERRO request', error);
+      })
+
+    this.RecipeService.getListFilter("type")
+      .then(apiResponse => {
+        this.listFilterType = apiResponse.data;
+      })
+      .catch(error => {
+        console.log('ERRO request', error);
+      })
+
     this.RecipeService.readAllItems()
       .then(apiResponse => {
-        this.allRecipe = apiResponse.data;
+        this.ObservablesService.getObservableData('recipe').subscribe( recipeList => {
+          this.allRecipe = recipeList;
+        })
       })
       .catch(error => {
         console.log('ERROR request', error);
       });
+
+      /*this.RecipeService.filterRecipe({
+        'filters': {
+          "ingredients": ["lait", "oeuf"],
+          "type": "",
+          "time": { "min_time": 1700, "max_time": 1800 },
+          "event": "",
+          "allergies": [],
+          "not_ingredients": [],
+          "level": "Facile",
+          "preferences": [],
+          "diet": ""
+        }
+      })
+      .then(apiResponse => {
+        this.ObservablesService.getObservableData('recipe').subscribe( recipeList => {
+          this.allRecipe = recipeList;
+        })
+      })
+      .catch(error => {
+        console.log('ERROR request', error);
+      });*/
   }
 
 }
